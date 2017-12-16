64ce24a76cb412d50e6f3886a35e782d3edb64f1
acrowfliedover <leofanrong@gmail.com>
2017-12-16 14:25:56 -0800

    too late to play around I guess

--- blocks.h
+++ blocks.h
@@ -42,23 +42,26 @@ public:
 };
 
 class Mino{
-private:
-    char type;
+protected:
     Cell space[4][4];
 public:
-    mino(char);
+    /*Mino(Mino_I &MinoI){
+    for (int i=0; i<4; ++i){
+        for (int j=0; j<4; ++j){
+            space[i][j]=MinoI[i][j];
+            }
+        }
+    }*/
     void rotate_left();
     void rotate_right();
 
 };
 
 class Mino_I: public Mino{
-private:
-    Cell I_space[4][4];
 public:
     Mino_I(){
         for (int i=0; i<4; ++i){
-            I_space[i][3].fill();
+            space[i][3].fill();
         }
     }
     void rotate_left();
@@ -66,8 +69,6 @@ public:
 };
 
 class Mino_S{
-private:
-    Cell S_space[4][4];
 public:
     Mino_S();
     void rotate_left();
@@ -75,8 +76,6 @@ public:
 };
 
 class Mino_L{
-private:
-    Cell L_space[4][4];
 public:
     Mino_L();
     void rotate_left();
@@ -84,8 +83,6 @@ public:
 };
 
 class Mino_T{
-private:
-    Cell T_space[4][4];
 public:
     Mino_T();
     void rotate_left();
@@ -93,8 +90,6 @@ public:
 };
 
 class Mino_O{
-private:
-    Cell O_space[4][4];
 public:
     Mino_O();
     void rotate_left();
@@ -102,8 +97,6 @@ public:
 };
 
 class Mino_J{
-private:
-    Cell J_space[4][4];
 public:
     Mino_J();
     void rotate_left();
@@ -111,8 +104,6 @@ public:
 };
 
 class Mino_Z{
-private:
-    Cell Z_space[4][4];
 public:
     Mino_Z();
     void rotate_left();
@@ -124,7 +115,7 @@ private:
     Line lines[20];
 public:
     void line_clear(int);
-    void move_left(Mino M);
+    virtual void move_left(Mino M);
     void move_right(Mino M);
     void move_down(Mino M);
 
